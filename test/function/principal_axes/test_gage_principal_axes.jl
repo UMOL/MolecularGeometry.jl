74598fc288aab1bbadc78cc286e20c9d2e122a9c
Yuhang(Steven) Wang <me@me>
2016-04-13 18:05:50 -0500

    rm redundant test

--- test/function/principal_axes/test_gage_principal_axes.jl
+++ test/function/principal_axes/test_gage_principal_axes.jl
@@ -50,9 +50,3 @@ test_gage_principal_axes(
      sqrt(2)/2. 0. -sqrt(2)/2.;
      sqrt(2)/2. 0. sqrt(2)/2. ], "test using array of arrays a input")
 
-test_gage_principal_axes(
-    Array[[0., 1., 1.],
-     [0., 0., 0.]], 
-    [0.  1.  0.;
-     sqrt(2)/2. 0. -sqrt(2)/2.;
-     sqrt(2)/2. 0. sqrt(2)/2. ], "test using array of arrays a input")
